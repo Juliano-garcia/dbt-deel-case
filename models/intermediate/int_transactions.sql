b118f6c03db763ca9ecb1b78562827f9b4a50b3d
Juliano Garcia <juliano.garcia@timeprimo.com>
2024-01-05 16:27:46 -0300

    Create int file

---
+++ models/intermediate/int_transactions.sql
@@ -0,0 +1,30 @@
+{{
+    config (
+        materialized = "view"
+    )
+}}
+
+WITH
+
+acceptance_report as ( SELECT * FROM {{ ref('stg_acceptance_report') }} )
+
+, chargeback_report as ( SELECT * FROM {{ ref('stg_chargeback_report') }} )
+
+SELECT
+    ar.external_ref
+    , ar.status
+    , ar.source
+    , ar.ref
+    , ar.date_time
+    , ar.state
+    , cr.chargeback
+    , ar.cvv_provided
+    , ar.country
+    , ar.currency
+    , ar.amount as amount_local_currency
+    , ar.amount / FLOAT64(ar.rates[ar.currency]) as amount_usd
+FROM
+    acceptance_report as ar
+LEFT JOIN
+    chargeback_report as cr
+    USING (external_ref)
