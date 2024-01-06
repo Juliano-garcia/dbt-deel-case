9730aa343dad4eebd54dc0452f3bc1ef2366ac54
Juliano Garcia <juliano.garcia@timeprimo.com>
2024-01-06 11:57:27 -0300

    Analysis to pull from mart table

--- analyses/missing_chargeback_data.sql
+++ analyses/missing_chargeback_data.sql
@@ -1,16 +1,10 @@
 
 WITH
 
-acceptance_report as ( SELECT * FROM {{ ref('stg_acceptance_report') }} )
-
-, chargeback_report as ( SELECT * FROM {{ ref('stg_chargeback_report') }} )
+transactions as ( SELECT * FROM {{ ref('mrt_transactions') }} )
 
 SELECT
-    ar.*
-    , cr.chargeback
+    *
 FROM
-    acceptance_report as ar
-LEFT JOIN
-    chargeback_report as cr
-    USING (external_ref)
-WHERE cr.chargeback IS NULL
+    transactions
+WHERE chargeback IS NULL
